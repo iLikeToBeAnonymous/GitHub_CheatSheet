e782e34e88f8c4a3b7e98388757b7776bb98c3ae
iLikeToBeAnonymous <49806693+iLikeToBeAnonymous@users.noreply.github.com>
2022-03-14 12:16:05 -0400

    Add files via upload

---
+++ Demo_PowerShell_Scripts/ZZZ_Open_List_of_URLs_in_Firefox_Private_browsing.ps1
@@ -0,0 +1,53 @@
+<#
+    .DESCRIPTION
+        Opens an array of URLs in an incognito Firefox window. If a private browsing window is not already open, one is opened.
+        Each url is opened as a new tab within the private browsing window.
+    .NOTES
+        Further Reading:
+            https://www.openallurls.com/
+            https://superuser.com/questions/1576302/start-chrome-with-a-url-with-options-incognito-mode-and-maximized-tab-in-batch
+            https://stackoverflow.com/questions/57027695/open-multiple-urls-with-firefox-in-private-mode-using-powershell
+            https://www.tdworld.com/overhead-distribution/article/21157216/scada-implementation-simplified
+        
+        Needed to do:
+            I would like to turn this into a function and have the function check if the variable passed is an array or a delimted string
+            to parse into an array.
+)
+#>
+$urlList1 = @(
+    'https://www.google.com/search?q=make+windows+deployment+image+from+existing+installation',
+    'https://docs.microsoft.com/en-us/windows-hardware/manufacture/desktop/deploy-a-custom-image?view=windows-11',
+    'https://www.google.com/search?q=sysprep+a+wim',
+    'https://askme4tech.com/sysprep-and-capture-windows-10-dism',
+    'https://community.spiceworks.com/topic/2224394-sysprep-win-pe',
+    'https://thesolving.com/server-room/when-and-how-to-use-sysprep/',
+    'https://www.youtube.com/watch?v=Bl1gNZCek4c',
+    'https://answers.microsoft.com/en-us/windows/forum/all/i-can-not-activate-windows-10-after-installing-a/345b2095-0d83-4450-bf49-b8476794028c',
+    'https://docs.microsoft.com/en-us/windows-hardware/manufacture/desktop/deploy-a-custom-image?view=windows-11',
+    'https://docs.microsoft.com/en-us/windows-hardware/manufacture/desktop/dism-image-management-command-line-options-s14?view=windows-11#capture-customimage',
+    'https://docs.microsoft.com/en-us/windows-hardware/manufacture/desktop/dism-configuration-list-and-wimscriptini-files-winnext?view=windows-11',
+    'https://docs.microsoft.com/en-us/windows/deployment/planning/windows-10-deprecated-features',
+    'https://github.com/FOGProject/fog-client/blob/master/Modules/HostnameChanger/Windows/WinActivation.cs',
+    'https://en.wikipedia.org/wiki/Installation_(computer_programs)'
+)
+
+$urlList2 = @(
+    'https://adamtheautomator.com/powershell-get-computer-name/',
+    'https://git-scm.com/book/en/v2/Git-Tools-Submodules',
+    'https://github.com/PowerShell/GPRegistryPolicy',
+    'https://www.atlassian.com/git/tutorials/git-submodule',
+    'https://stackoverflow.com/questions/1260748/how-do-i-remove-a-submodule',
+    'https://stackoverflow.com/questions/1260748/how-do-i-remove-a-submodule/36593218#36593218',
+    'https://stackoverflow.com/questions/4089430/how-can-i-determine-the-url-that-a-local-git-repository-was-originally-cloned-fr',
+    'https://git-scm.com/docs/git-mv',
+    'https://git-scm.com/docs/gitignore'
+)
+
+
+
+
+# $sysPath = $env:Path.Split(',')
+# Start-Process firefox -ArgumentList @('-private-window' , 'https://youtube.com/')
+$urlList2 | ForEach-Object{
+    Start-Process firefox -ArgumentList @('-private-window' , $_)
+}
